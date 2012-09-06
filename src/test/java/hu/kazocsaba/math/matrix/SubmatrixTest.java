3a545e1f1ff2925843fe29b482bf3f12a12aba4f
Kazó Csaba <kazocsaba@gmail.com>
2012-09-06 10:36:50 +0200

    Add some tests for views.

--- src/test/java/hu/kazocsaba/math/matrix/SubmatrixTest.java
+++ src/test/java/hu/kazocsaba/math/matrix/SubmatrixTest.java
@@ -1,5 +1,7 @@
 package hu.kazocsaba.math.matrix;
 
+import hu.kazocsaba.math.matrix.immutable.ImmutableMatrix;
+import hu.kazocsaba.math.matrix.immutable.ImmutableMatrixFactory;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -29,6 +31,71 @@ public class SubmatrixTest {
 		check(m.getSub().vector2(0, 0), 1, 6);
 		check(m.getSub().vector3(1, 2), 8, 13, 18);
 		check(m.getSub().vector4(0, 3), 4, 9, 14, 19);
+	}
+	
+	@Test
+	public void testViews() {
+		ImmutableMatrix reference=ImmutableMatrixFactory.createMatrix(new double[][]{
+			{1,   2,  3,  4,  5},
+			{6,   7,  8,  9, 10},
+			{11, 12, 13, 14, 15},
+			{16, 17, 18, 19, 20}
+		});
+		
+		{
+			Matrix m=MatrixFactory.copy(reference);
+			Vector view=m.viewSub().column(4);
+			check(view, 5, 10, 15, 20);
+			view.scale(-1);
+			check(view, -5, -10, -15, -20);
+			check(m, new double[][]{
+				{1,   2,  3,  4,  -5},
+				{6,   7,  8,  9, -10},
+				{11, 12, 13, 14, -15},
+				{16, 17, 18, 19, -20}
+			});
+			m.scale(-1);
+			check(view, 5, 10, 15, 20);
+		}
+		{
+			Matrix m=MatrixFactory.copy(reference);
+			Matrix view=m.viewSub().matrix(2, 0, 2, 3);
+			check(view, new double[][]{
+				{11, 12, 13},
+				{16, 17, 18}
+			});
+			view.scale(-1);
+			check(view, new double[][]{
+				{-11, -12, -13},
+				{-16, -17, -18}
+			});
+			check(m, new double[][]{
+				{1,     2,   3,  4,  5},
+				{6,     7,   8,  9, 10},
+				{-11, -12, -13, 14, 15},
+				{-16, -17, -18, 19, 20}
+			});
+			m.scale(-1);
+			check(view, new double[][]{
+				{11, 12, 13},
+				{16, 17, 18}
+			});
+		}
+		{
+			Matrix m=MatrixFactory.copy(reference);
+			Vector3 view=m.viewSub().rowVector3(3, 2);
+			check(view, 18, 19, 20);
+			view.scale(-1);
+			check(view, -18, -19, -20);
+			check(m, new double[][]{
+				{1,   2,   3,   4,   5},
+				{6,   7,   8,   9,  10},
+				{11, 12,  13,  14,  15},
+				{16, 17, -18, -19, -20}
+			});
+			m.scale(-1);
+			check(view, 18, 19, 20);
+		}
 	}
 	
 	private static void check(Vector v, double... elements) {
